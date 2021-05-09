568949499237985e4af1bccd63cb9a685dfaf7bc
Boris A. Burkov <vasjaforutube@gmail.com>
2021-05-09 11:03:10 +0300

    Kafka in docker-compose is glitching; let's restart docker-compose on every test suite run.

--- kafka_store_test.go
+++ kafka_store_test.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -26,8 +27,6 @@ func (suite *KafkaAdapterTestSuite) SetupSuite() {
 	suite.Zookeeper = "zookeeper:2181"
 	suite.ConsumerGroup = "goConsumerGroup"
 
-	fmt.Println("SetupTest()")
-
 	// initialize topic
 	cmd := exec.Command("docker", "exec", "broker", "kafka-topics", "--create", "--topic", suite.Topic, "--bootstrap-server", suite.BootstrapServer, "--replication-factor", "1", "--partitions", "1")
 	err := cmd.Run()
@@ -38,36 +37,49 @@ func (suite *KafkaAdapterTestSuite) SetupSuite() {
 }
 
 func (suite *KafkaAdapterTestSuite) TearDownSuite() {
-	fmt.Println("TearDown()")
-
 	// destroy Topic
-	cmd := exec.Command("docker", "exec", "broker", "kafka-topics", "--zookeeper", suite.Zookeeper, "--delete", "--topic", suite.Topic)
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
+	// cmd := exec.Command("docker", "exec", "broker", "kafka-topics", "--zookeeper", suite.Zookeeper, "--delete", "--topic", suite.Topic)
+	// err := cmd.Run()
+	// if err != nil {
+	// panic(err)
+	// }
 }
 
 func (suite *KafkaAdapterTestSuite) TestLoadMeta() {
 	var ctx context.Context = context.Background()
 	var kafkaStore *KafkaStore = NewKafkaStore(suite.BootstrapServer, suite.Topic, suite.ConsumerGroup)
 
-	var callback = func(reader io.Reader) error {
-		for {
-			buffer := make([]byte, 1000000)
-			bytesRead, err := reader.Read(buffer)
-			buffer = buffer[0 : bytesRead+1]
-
-			if err != nil {
-				return err
-			}
-		}
+	// produce a message 'foobar' using kafka console client
+	command := fmt.Sprintf("bash \"foobar\" | docker exec broker kafka-console-producer --bootstrap-server %s --topic %s", suite.BootstrapServer, suite.Topic)
+	cmd := exec.Command("bach", "-c", command)
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
 	}
 
-	err := kafkaStore.LoadMeta(ctx, callback)
+	data := make([]byte, 1000000)
+	var callback = func(reader io.Reader) error {
+		buffer := make([]byte, 1000000)
+		for {
+			bytesRead, err := reader.Read(buffer)
+			if err == io.EOF {
+				break
+				data = append(data, buffer[:bytesRead+1]...)
+			} else if err != nil {
+				return err
+			} else {
+				data = append(data, buffer[:bytesRead+1]...)
+			}
+		}
+		return nil
+	}
+
+	err = kafkaStore.LoadMeta(ctx, callback)
 	if err != nil {
 		suite.T().Error("TestLoadMeta() error")
 	}
+
+	assert.Equal(suite.T(), "foobar", string(data), "Received message should be the same as produced by console producer")
 }
 
 func (suite *KafkaAdapterTestSuite) TestAppendMeta() {
@@ -88,6 +100,15 @@ func (suite *KafkaAdapterTestSuite) TestAppendMeta() {
 		suite.T().Error("TestAppendMeta() error")
 		// t.Errorf("TestAppendMeta() error: %s\n", err)
 	}
+
+	// consume the message with console consumer, make sure that it is the same as we passed on input
+	cmd := exec.Command("docker", "exec", "broker", "kafka-console-consumer", "--from-beginning", "--bootstrap-server", suite.BootstrapServer, "--topic", suite.Topic, "--timeout-ms", "15000")
+	output, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("TestAppendMeta(): cmd.Output = %s", output)
+	assert.Equal(suite.T(), "This is a programmatically produced message", string(output), "Console consumer should receive the produced message")
 }
 
 func (suite *KafkaAdapterTestSuite) TestReplaceMeta() {
